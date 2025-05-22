495f3a4979834b838227640660f7381086964849
Maurice Taylor <mata@Maurices-MacBook-Pro.local>
2025-05-22 13:34:13 +0100

    Test: Simplify index.js to plain JS

---
+++ index.js
@@ -0,0 +1,24 @@
+
+console.log("index.tsx script started!");
+
+// Temporarily comment out all React-specific code.
+// We want to see if the browser can even load and parse this file as a module.
+
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import App from './App';
+
+// const rootElement = document.getElementById('root');
+// if (!rootElement) {
+//  console.error("Could not find root element to mount to");
+// } else {
+//  console.log("Root element found:", rootElement);
+//  // const root = ReactDOM.createRoot(rootElement);
+//  // root.render(
+//  //   <React.StrictMode>
+//  //     <App />
+//  //   </React.StrictMode>
+//  // );
+// }
+
+console.log("index.tsx script finished (simplified version).");
